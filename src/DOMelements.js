c97fcb71c05e00157bb214e5f0af928a1d77972f
saimaAnis21 <saima_rehman21@yahoo.com>
2021-05-03 01:02:15 +0500

    Display API data on page

--- src/DOMelements.js
+++ src/DOMelements.js
@@ -5,6 +5,7 @@ const form = document.createElement('form');
 form.setAttribute('method','post');
 
 const cityinput = document.createElement('input');
+cityinput.setAttribute('class','mr-10');
 cityinput.setAttribute('type','text');
 cityinput.setAttribute('id','cityinput');
 cityinput.setAttribute('name','city');
@@ -13,7 +14,7 @@ cityinput.setAttribute('required','true');
 
 const citybtn = document.createElement('input');
 citybtn.setAttribute('type','submit');
-citybtn.value="Submit";
+citybtn.value="Weather Data";
 
 form.appendChild(cityinput);
 form.appendChild(citybtn);
@@ -21,6 +22,30 @@ form.appendChild(citybtn);
 const datadiv = document.createElement("div");
 datadiv.setAttribute("class","flex-col");
 
+const tempunitbtn = document.createElement("button");
+tempunitbtn.innerHTML="Convert to &#8457;";
+
+const cityspan = document.createElement("span");
+cityspan.setAttribute("class","item");
+const iconimg = document.createElement("img");
+iconimg.setAttribute("class","weathericon hidden item");
+const tempspan = document.createElement("span");
+tempspan.setAttribute("class","item");
+const mintempspan = document.createElement("span");
+mintempspan.setAttribute("class","item");
+const maxtempspan = document.createElement("span");
+maxtempspan.setAttribute("class","item");
+const weatherspan = document.createElement("span");
+weatherspan.setAttribute("class","item");
+
+datadiv.appendChild(tempunitbtn);
+datadiv.appendChild(cityspan);
+datadiv.appendChild(iconimg);
+datadiv.appendChild(tempspan);
+datadiv.appendChild(mintempspan);
+datadiv.appendChild(maxtempspan);
+datadiv.appendChild(weatherspan);
+
 export {
-    errordiv, cityinput, citybtn, datadiv, form
+    errordiv, cityinput, citybtn, datadiv, form, tempspan, mintempspan, maxtempspan, weatherspan, cityspan, iconimg, tempunitbtn
   };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import { errordiv, cityinput, citybtn, datadiv, form } from './DOMelements';
+import { errordiv, cityinput, citybtn, datadiv, cityspan,tempspan, mintempspan, maxtempspan, weatherspan, iconimg, tempunitbtn} from './DOMelements';
 import { getData } from './weatherinfo';
-//require('dotenv');
+//require('dotenv').config;
 //console.log(process.env.API_KEY);
 
-  const weatherdiv = document.getElementById("weatherdata");
+let citydata ="";
+let icon=""
+let temp="";
+let mintemp="";
+let maxtemp="";
+let weather = "";
+let desc="";
+let feelslike= "";
 
+  let weatherdiv = document.getElementById("weatherdata");
   weatherdiv.appendChild(errordiv);
   weatherdiv.appendChild(cityinput);
   weatherdiv.appendChild(citybtn);
   weatherdiv.appendChild(datadiv);
 
-  let weatherdata = "";
+  const populatedata = () => {
+    cityspan.innerHTML= `City: ` + `${citydata}`; 
+    iconimg.src = ` http://openweathermap.org/img/wn/${icon}@2x.png`;
+    
+    tempspan.innerHTML= `Temperature: ` + `${temp} &#8451;` + `, feels like ${feelslike} &#8451;`;
+    mintempspan.innerHTML= `Minimum Temperature: ` + `${mintemp} &#8451;`;
+    maxtempspan.innerHTML= `Maximum Temperature: ` + `${maxtemp} &#8451;`;
+    weatherspan.innerHTML= `Weather: ` + `${weather}` + `, ${desc}`;
+  }
+
+
   citybtn.addEventListener('click', async () => {
-    let city = "";
+    let city ="";
     if(cityinput.value===""){
       city = "karachi";
     }
@@ -20,10 +38,18 @@ import { getData } from './weatherinfo';
       city = cityinput.value;
     }
     const dataobj = await getData(city);
-    const data = dataobj;
-    weatherdata = data["main"].temp;
-    datadiv.innerText = weatherdata;
-
+    const data = await dataobj;
+    console.log(data);
+     citydata = await data["name"];
+     icon = data["weather"][0].icon;
+     temp = data["main"].temp;
+     mintemp = data["main"].temp_min;
+     maxtemp = data["main"].temp_max;
+     weather=data["weather"][0].main;
+     desc=data["weather"][0].description;
+     feelslike = data["main"].feels_like;
+     iconimg.setAttribute("class","weathericon visible");
+     populatedata();
   });
 
-
+ 
